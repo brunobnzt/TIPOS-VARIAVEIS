78da72ed2b812c9fca36d43496d4c14d119f73fe
Bruno <bruno.bnzt@gmail.com>
2024-05-15 13:04:25 -0300

    Update TiposVariaveis.java

--- TiposVariaveis/src/TiposVariaveis.java
+++ TiposVariaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        int numero = 10; // integer
+        float salario = 1000.50f; // float
+        double altura = 1.75; // double
+        char letra = 'A'; // character
+        boolean verdadeiro = true; // boolean
+
         System.out.println("Hello, World!");
     }
 }
